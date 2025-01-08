fddf7494fbe7ab8bfacc736320983d738f2eb739
Yanan Wang <156512906+00mmw@users.noreply.github.com>
2025-01-08 12:54:59 +0800

    Update main.py
    speed

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
     port = "COM9"  # Change to your COM port
     gripper = GripperControl(port)
     reset_position = 100 #default
-    close_position= 300
+    close_position = 200
+    speed = 100
     
     # Example usage
+    gripper.set_speed(speed)
     gripper.gripper_close(close_position)  # Close grippers to position range is [100, 555], max=560
-    gripper.gripper_reset(reset_position)  # Open grippers to default position (100)
-    gripper.read_position()  # Read current positions
+    #gripper.gripper_reset(reset_position)  # Open grippers to default position (100)
+    #gripper.read_position()  # Read current positions
