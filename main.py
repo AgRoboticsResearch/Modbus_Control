a134cb0aa4db4146e71abdcae33f3aa8ed3c6a97
Yanan Wang <156512906+00mmw@users.noreply.github.com>
2025-01-15 17:06:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ if __name__ == "__main__":
     gripper.gripper_close(close_position)  # Close grippers to position range is [100, 555], max=560
     #gripper.gripper_reset(reset_position)  # Open grippers to default position (100)
     #gripper.read_position()  # Read current positions
+    # gripper.read_PID(key="P")   #20
+    # gripper.read_PID(key="I")   #0
+    # gripper.read_PID(key="D")   #0
+    # gripper.set_PID(key="P", value=50)
+    # gripper.set_PID(key="D", value=0)
+    # gripper.set_PID(key="I", value=5)
